b9642a5665c70f3c350b362435b416c3f569f0c4
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-06 18:45:30 +0000

    Assign a random value to a variable and say if it's positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%u is positive\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%u is zero\n", n);
+		printf("%i is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }

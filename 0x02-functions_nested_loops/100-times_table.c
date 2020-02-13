0841f4ee486699ad159892d912b1eca902e39568
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-13 04:54:49 +0000

    Print table of integers between 0 and 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,48 @@
 #include "holberton.h"
 /**
-  * print_times_table - check the code for Holberton School students.
-  * @n: integer number between 0 and 15.
-  */
+ * print_times_table - check the code for Holberton School students.
+ * @n: integer number between 0 and 15.
+ */
 void print_times_table(int n)
 {
 	int colum, filas, result;
 
-	for (filas = 0; filas <= n && n > 0 && n < 15; filas++)
+	if (n >= 0 && n <= 15)
 	{
-		for (colum = 0; colum <= n; colum++)
+		for (filas = 0; filas <= n; filas++)
 		{
-			result = colum * filas;
-			if (colum == 0)
+			for (colum = 0; colum <= n; colum++)
 			{
-				_putchar(result + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (result < 10)
+				result = colum * filas;
+				if (colum == 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
 					_putchar(result + '0');
 				}
-				else if (result > 9 && result < 100)
+				else
 				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else if (result > 99)
-				{
-					_putchar((result / 100) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
+					if (result < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(result + '0');
+					}
+					else if (result > 9 && result < 100)
+					{
+						_putchar(' ');
+						_putchar((result / 10) + '0');
+						_putchar((result % 10) + '0');
+					}
+					else if (result > 99)
+					{
+						_putchar((result / 100) + '0');
+						_putchar(((result / 10) % 10) + '0');
+						_putchar((result % 10) + '0');
+					}
 				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }

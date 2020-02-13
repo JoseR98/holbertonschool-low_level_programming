043b5a571f29f2d1f7ac431af18f17166faf8e06
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-13 20:35:31 +0000

    Print numbers from 1 to 100 (FizzBuzz task)

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+  * main - print number from 0 to 100.
+  * Return: always 0.
+  */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", n);
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

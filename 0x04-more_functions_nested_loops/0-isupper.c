2dd626295de08e5226c84a72aefd711001272fd3
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-13 15:27:41 +0000

    Check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  * _isupper - Checks for upperchase character.
+  *
+  *@c: integer parameter.
+  *
+  * Return: 1 if is uppercase 0 otherwise.
+  */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

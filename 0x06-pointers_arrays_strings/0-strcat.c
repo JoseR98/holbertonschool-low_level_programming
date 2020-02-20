87944d63a5c8e2d6a373e4657b8e00ac2ba7ea98
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-20 18:32:35 +0000

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strcat - Concatenate two strings
+ *
+ * @dest: first string
+ *
+ * @src: second string
+ *
+ * Return: char type
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;/* Take the number of characters in the string*/
+
+	while (*(dest + i) != '\0')/* determine the number of characters*/
+	{
+		i++;
+	}
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}

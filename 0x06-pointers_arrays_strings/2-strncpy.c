e0ac581cb400b2375c9f2ea94eb38891e623644b
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-20 19:35:16 +0000

    Create a copy of a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,17 +13,17 @@ char *_strncpy(char *dest, char *src, int n)
 	
 	while (j < n && *(src + j) != '\0')
 	{
-		if (*(src + j) != '\n')
-		{
-			*(dest + i) = *(src + j);
-		}
-		else
-		{
-			*(dest + i) = '\n';
-			*(dest + i + 1) = '\0';
-		}
+		
+		*(dest + i) = *(src + j);
+
+
 		i++;
 		j++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
 	}
 	return (dest);
 }

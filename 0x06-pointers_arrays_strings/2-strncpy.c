08ae2dfcfd196b59ffad62bd5f59302de08c19a2
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-20 19:42:08 +0000

    Create a copy of a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,19 @@
 #include "holberton.h"
-
 /**
- * _strncpy -
- *
- * Return: 
+ * _strncpy - Copy a string in another string
+ * @dest: first string
+ * @src: second string
+ * @n:number of bytes
+ * Return: char pointer
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
 
-	
 	while (j < n && *(src + j) != '\0')
 	{
-		
 		*(dest + i) = *(src + j);
-
-
 		i++;
 		j++;
 	}

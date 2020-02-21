9709be651d7f8a3b3500d9fc86992124d3ddff4d
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-21 00:06:12 +0000

    Capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Change from lowercase to uppercase
+ * @s: pointer to array of characters.
+ * Return: array of characters
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	char p[12];
+
+	p[0] = ' ';
+	p[1] = '\t';
+	p[2] = '\n';
+	p[3] = ',';
+	p[4] = ';';
+	p[5] = '.';
+	p[6] = '!';
+	p[7] = '?';
+	p[8] = '"';
+	p[9] = '(';
+	p[10] = ')';
+	p[11] = '{';
+	p[12] = '}';
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; p[j] != '\0'; j++)
+		{
+			if (*(s + i) == p[j])
+			{
+				if (*(s + i + 1) > 96 && *(s + i + 1) < 123)
+				{
+					*(s + i + 1) = *(s + i + 1) - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}

500937e64d85a6047b00907a509344ebe684e2ed
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-21 01:38:51 +0000

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,7 @@ char *cap_string(char *s)
 	int j;
 	char p[] = " \t\n,;.!\?\"(){}";
 
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; p[j] != '\0'; j++)
@@ -21,9 +22,12 @@ char *cap_string(char *s)
 				{
 					s[i + 1] = s[i + 1] - 32;
 				}
-				else
+			}
+			else if (i == 0)
+			{
+				if (s[i] > 96 && s[i] < 123)
 				{
-					s[i + 1] = s[i + 1];
+					s[i] = s[i] -32;
 				}
 			}
 		}

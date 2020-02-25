238e86903e14fbeb8267e5f03409a541f0f3b546
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-25 02:14:39 +0000

    locate a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strstr - function finds the first occurrence of a substring 
+ * _strstr - function finds the first occurrence of a substring
  * @haystack: array principal
  * @needle: secondary array
  * Return: pointer to first ocurrence
@@ -10,6 +10,7 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack != 0)
 	{
 		char *new_position = haystack;
+
 		while (*haystack == *needle && *haystack != 0 && *needle != 0)
 		{
 			needle++;

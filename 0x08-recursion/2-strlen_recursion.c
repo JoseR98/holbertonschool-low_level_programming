0150de42af1272ce7ff569f1af5c573ab842c945
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-26 23:36:20 +0000

    Return length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * _strlen_recursion -
- *
- * Return: 
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to first character of string
+ * Return: integer value
  */
 int _strlen_recursion(char *s)
 {
-
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
 }

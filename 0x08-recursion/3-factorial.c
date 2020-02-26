1c4bff491561117ed138101d6235815999ab2532
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-26 23:36:51 +0000

    Return the factorial of a integer

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * factorial -
- *
- * Return: 
+ * factorial - Returns factorial of a number
+ * @n: integer parameter
+ * Return: integer value
  */
 int factorial(int n)
 {
-
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
 }

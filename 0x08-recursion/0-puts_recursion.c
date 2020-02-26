edc7960240a8bc7661c5324f93b43059d3f4ec78
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-26 23:35:13 +0000

    Print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion -
- *
- * Return: 
+ * _puts_recursion - print a string
+ * @s: pointer to the first character of s
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }

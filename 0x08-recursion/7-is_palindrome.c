8b6cd585bb503a512561c5d371c7fca9cc3be8c7
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-27 03:32:10 +0000

    Determine if is palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 int evaluate(int i, int j, char *s);
+int _strlen_recursion(char *s);
 /**
  * is_palindrome - determine if a string is palindrome
  * @s: string given
@@ -35,6 +36,22 @@ int evaluate(int i, int j, char *s)
 	else if (j <= i)
 	{
 		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to first character of string
+ * Return: integer value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{

e8dc163ad125a07eee9638de1d44227a9c55a637
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-27 00:30:50 +0000

    Return square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,42 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion -
- *
- * Return: 
+ * _raiz - find square root of a number
+ * @x: integer value (sqrt)
+ * @n: integer value
+ * Return: integer value (square root)
+ */
+int _raiz(int x, int n)
+{
+	if ((x * x) == n)
+	{
+		return (x);
+	}
+	else if ((x * x) > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_raiz((x + 1), n));
+	}
+}
+/**
+ * _sqrt_recursion - Return the square root of a number
+ * @n: integer value
+ * Return: integer value
  */
 int _sqrt_recursion(int n)
 {
+	int result, x = 0;
 
+	if (n > 0)
+	{
+		result = _raiz(x, n);
+		return (result);
+	}
+	else
+	{
+		return (-1);
+	}
 }

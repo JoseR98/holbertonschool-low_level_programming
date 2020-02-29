c165a1f5cf78156d5bfc24c5b2eca7365807e425
JoseR98 <josealonsoingeniero@hotmail.com>
2020-02-29 20:25:46 +0000

    Print program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: count the number of arguments passed to the program
+ * @argv: array that contains the arguments of the program
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

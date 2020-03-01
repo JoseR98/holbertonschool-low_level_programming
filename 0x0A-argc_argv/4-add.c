3457b5e44c11d57796c5576b09bdb9a40886dbfc
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-01 00:00:48 +0000

    Add arguments of a program

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,25 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 /**
  * main - prints the sum of arguments of a program
  * @argc: count of arguments in the program
  * @argv: array of arguments of the program
  * Return: 1 if an error occurs and 0 if succes
  */
+int number(char *s);
+
 int main(int argc, char *argv[])
 {
 	int sum, i;
-	
-	(void)argc;
+
 	if (argv[1] == '\0')
 	{
 		printf("0\n");
 	}
 	else
 	{
-		for (i = 1; argv[i] != '\0'; i++)
+		for (i = 1; i < argc; i++)
 		{
-			if (! atoi(argv[i]) % 10 )
+			if (number(argv[i]))/* determine if the argument is a number*/
 			{
 				printf("Error\n");
 				return (1);
@@ -32,4 +34,23 @@ int main(int argc, char *argv[])
 		printf("%d\n", sum);
 	}
 	return (0);
+}
+/**
+ *
+ *
+ *
+ *
+ */
+int number(char *s)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if (isdigit(s[j]))
+		{
+			return (0);
+		}
+	}
+	return (1);
 }

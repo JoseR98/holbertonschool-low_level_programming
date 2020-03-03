3bef41cdc2e05452f68af3d752475f0c25e3aa29
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-03 18:09:52 +0000

    Create an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,11 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		p = malloc(sizeof(char)*size);
+		p = malloc(sizeof(char) * size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
 		for (i = 0; i < size; i++)
 		{
 		p[i] = c;

ef482be8adfc7a4e37ac9f95ee70992c86737639
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-04 01:37:59 +0000

    Return a pointer to a 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,7 @@ int **alloc_grid(int width, int height)
 	p = malloc(sizeof(p) * height);
 	if (p == NULL)
 	{
+            free(p);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -25,6 +26,7 @@ int **alloc_grid(int width, int height)
 		p[i] = malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
+                  free(p[i]);
 			return (NULL);
 		}
 	}

515c951d166577572cd351a2a16491addbc41f6e
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-04 18:20:58 +0000

    Split a string into words

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-int palabras (char *str);
+int palabra(char *str);
 /**
  * strtow - divide string
  * @str: string
@@ -8,18 +8,21 @@ int palabras (char *str);
 char **strtow(char *str)
 {
 	char **p;
-	int i, j = 0, letras, palabras = 0, columnas, filas;/*num_p número de palabras; num_l número de letras en la palabra*/
+	int j = 0, letras, palabras = 0, columnas, filas;/*num_p número de palabras; num_l número de letras en la palabra*/
 
 	if (str == NULL || str == '\0')
 	{
 		return (NULL);
 	}
-	palabras = palabras(str);/* determina el número de palabras en el string*/
-	p = malloc(sizeof(p) * palabras);
-
-	for (filas = 0; filas < palabras; filas++)/* determina el número de letras de cada palabra y se asigna una ubicacion de memoria para la cantidad de letras*/
+	palabras = palabra(str);/* determina el número de palabras en el string*/
+	p = malloc(sizeof(p) * palabras + 1);/* guarda espacio de memoria para almacenar las palabras*/
+	if (p == NULL)
 	{
-		while(str[j] != '\0')
+		return (NULL);
+	}
+	for (filas = 0; filas < palabras + 1; filas++)/* determina el número de letras de cada palabra y se asigna una ubicacion de memoria para la cantidad de letras*/
+	{
+		while (str[j] != '\0')
 		{
 			if ((str[j] > 64 && str[j] < 91) || (str[j] > 96 && str[j] < 123))
 			{
@@ -30,10 +33,21 @@ char **strtow(char *str)
 				else
 				{
 					letras++;
-					p[filas] = malloc(sizeof(char) * letras);
-					for (columnas = 0; columnas < letras; columnas++)
+					p[filas] = malloc(sizeof(char) * letras);/* asigna a la posición filas el número de letras que va a tener*/
+					if (p[filas] == NULL)
 					{
-						p[filas][columnas] = str[j + 1 + columnas - letras];
+						return (NULL);
+					}
+					for (columnas = 0; columnas <= letras; columnas++)/* asigna letra por letra a cada posición de mi array*/
+					{
+						if (columnas < letras)
+						{
+							p[filas][columnas] = str[j + 1 + columnas - letras];
+						}
+						else if (columnas == letras)
+						{
+							p[filas][columnas] = '\0';
+						}
 					}
 					letras = 0;
 					j++;
@@ -50,7 +64,7 @@ char **strtow(char *str)
  *
  *
  */
-int palabras (char *str)
+int palabra(char *str)
 {
 	int i, palabras = 0;
 

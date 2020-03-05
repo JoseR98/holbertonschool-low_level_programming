80bbe70289da936a6be6cc0a728c17c8a7ef7a85
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-05 19:53:20 +0000

    Concatenate one string with n bytes of other string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
-unsigned int size_s1(char *s1);
-unsigned int size_s2(char *s2);
-void fill_all(char *s1, char *s2, char *p, unsigned int size1);
+void fill_all(char *s1, char *s2, char *p, unsigned int size1, unsigned int size2);
 void fill_n_bytes(char *s1, char *s2, char *p, unsigned int size1, unsigned int n);
+unsigned int _strlen(char *s);
 /**
  * string_nconcat - concatenate first string with n bytes from the second string
  * @s1: pointer to first string
@@ -14,64 +13,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 
-	if (n >= size_s2(s2))
+	if (s1 == NULL)
+	{	
+		s1 = malloc(1);
+		*s1 = '\0';
+	}
+	if (s2 == NULL)
+	{	
+		s2 = malloc(1);
+		*s2 = '\0';
+	}
+	if (n >= _strlen(s2))
 	{
-		p = malloc(sizeof(char) * (size_s1(s1) + size_s2(s2) - 1));
+		p = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 		if (p == NULL)
-		{
 			return (NULL);
-		}
-		fill_all(s1, s2, p, size_s1(s1));
+		fill_all(s1, s2, p, _strlen(s1), _strlen(s2));
 		return (p);
 	}
 	else
-	{
-		p = malloc(sizeof(char) * (size_s1(s1) + n));
+		p = malloc(sizeof(char) * (_strlen(s1) + n + 1));
 		if (p == NULL)
-		{
 			return (NULL);
-		}
-		fill_n_bytes(s1, s2, p, size_s1(s1), n);
+		fill_n_bytes(s1, s2, p, _strlen(s1), n);
 		return (p);
-	}
-}
-/**
- * size_s1 - determine the size of a string
- * @s1: pointer to a string
- * Return: return the size of a string
- */
-unsigned int size_s1(char *s1)
-{
-	unsigned int size1 = 0;
-
-	if (s1 == NULL)
-	{
-		s1[size1] = '\0';
-	}
-	while (s1[size1] != '\0')
-	{
-		size1++;
-	}
-	return (size1);
-}
-/**
- * size_s2 - determine the size of s2
- * @s2:input pointer to a string
- * Return: size of string 2
- */
-unsigned int size_s2(char *s2)
-{
-	unsigned int size2 = 0;
-
-	if (s2 == NULL)
-	{
-		s2[size2] = '\0';
-	}
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
-	return (size2);
 }
 /**
  * fill_all - concatenates both strings
@@ -80,19 +45,19 @@ unsigned int size_s2(char *s2)
  * @p: pointer to the new memory allocation
  * @size1: size of first string
  */  
-void fill_all(char *s1, char *s2, char *p, unsigned int size1)
+void fill_all(char *s1, char *s2, char *p, unsigned int size1, unsigned int size2)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, j = 0;
 
-	while (s1[i] != '\0')
+	while (i < size1)
 	{
 		p[i] = s1[i];
 		i++;
 	}
-	while (s2[i - size1] != '\0')
-	{
-		p[i] = s2[i - size1];
+	while (j < size2)
+	{	p[i] = s2[j];
 		i++;
+		j++;
 	}
 	p[i] = '\0';
 }
@@ -106,17 +71,29 @@ void fill_all(char *s1, char *s2, char *p, unsigned int size1)
  */
 void fill_n_bytes(char *s1, char *s2, char *p, unsigned int size1, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, j = 0;
 
-	while (s1[i] != '\0')
-	{
-		p[i] = s1[i];
+	while (i < size1)
+	{	p[i] = s1[i];
 		i++;
 	}
-	while ((i- size1) < n)
-	{
-		p[i] = s2[i - size1];
+	while (j < n)
+	{	p[i] = s2[j];
 		i++;
+		j++;
 	}
 	p[i] = '\0';
+}
+/**
+ *
+ *
+ *
+ */
+unsigned int _strlen(char *s)
+{	
+	unsigned int i = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
 }

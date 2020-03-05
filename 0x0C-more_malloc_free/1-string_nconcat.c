bd5c70b6c39e910e4f818ba1b57175339be50484
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-05 21:38:59 +0000

    Concatenate one string with n bytes of other string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size1++;
 	while (*(s2 + size2))
 		size2++;
-	if (n < size2)
+	if (n <= size2)
 		size2 = n;
 	p = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (p == NULL)

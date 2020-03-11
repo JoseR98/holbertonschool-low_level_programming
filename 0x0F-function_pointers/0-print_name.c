4de5f1c57645fd8a4c0c7e0c5b9d045e779be9fd
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-11 17:30:04 +0000

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - print name
+ * @name: name parameter
+ * @f: pointer to function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

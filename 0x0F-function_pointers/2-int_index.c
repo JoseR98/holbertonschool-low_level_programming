6e7e203bf4f895484e3f6c8e6ed9c08dcf3d210c
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-11 18:59:15 +0000

    Search for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include <stdio.h>
 /**
  * int_index - search for an integer
  * @array: array of integers
@@ -16,6 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	else
 	{
+		if (cmp == NULL || array == NULL)
+			return (-1);
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))

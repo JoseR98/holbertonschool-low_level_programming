64b64b2d72e73c67647cf8a3bfe57a60e8bbb33d
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-14 17:41:28 +0000

    Fix to variadic_functions header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,10 +9,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct format - structure of operator
+ * @symbol: char pointer
+ * @p: pointer to function
+ */
 typedef struct format
 {
 	char *symbol;
 	void (*p)();
-}f_t;
+} f_t;
 #endif /* VARIADIC_FUNCTIONS_H */

fe0d00682fd81f89aa856f23d849968cff449835
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-19 21:11:57 +0000

    Add new node to a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - add node to a linked list at the beginning
+ * @head: address passed by reference
+ * @str: value for the new node
+ * Return: new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *s = strdup(str);
+	list_t *new;
+	int len_str;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	for (len_str = 0; s[len_str] != '\0'; len_str++)
+		;
+	new->str = s;
+	new->len = len_str;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+

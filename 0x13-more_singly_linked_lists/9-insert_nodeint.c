2d3d099380e122160f09a5c7904a762da31e8a12
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-24 16:39:08 +0000

    Hot fix 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new_pos == NULL || head == NULL)
 		return (NULL);
+	
 	new_pos->n = n;
+
 	if (idx == 0)
 	{
 		new_pos->next = *head;

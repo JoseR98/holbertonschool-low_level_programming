af623c083d6fd77fd222543dd23acb327b992160
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-25 00:50:37 +0000

    Print list safe version

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints elements of a list
+ * @head: pointer to the first node of list
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int num_nodes;
+
+	if (head == NULL)
+		return (0);
+	for (num_nodes = 0; head != NULL; num_nodes++)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head->next > head)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, (head->next)->n);
+			exit(98);
+		}
+		head = head->next;
+	}
+	return (num_nodes);
+}

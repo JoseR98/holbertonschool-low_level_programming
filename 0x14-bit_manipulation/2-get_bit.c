881bec0469bed702995864885c536eab3079b909
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-26 23:28:51 +0000

    Add conditional statement

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
-
 /**
  * get_bit - get bit at a given index
- * @n:unsigned integer given value 
+ * @n:unsigned integer given value
  * @index: index of binary number
- * Return: 
+ * Return: bit at index given
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+    if (n == 0 && index > 0)
+        return (-1);
+    if (!(n >> index))
+        return (-1);
     return ((n >> index) & 1);
 }

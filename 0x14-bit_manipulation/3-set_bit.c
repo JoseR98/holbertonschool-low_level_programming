bc2cb41bb5b488ea93778432f980f16919c0e2c9
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-27 01:03:57 +0000

    Set bit at given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * set_bit - set the value of a bit at a given index
+ * @n: pointer to variable's address
+ * @index: index for setting the bit
+ * Return: integer value
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bit = 1;
+
+	if (index > (sizeof(unsigned long int) * 8) - 1)
+		return (-1);
+	if (n == NULL)
+		return (-1);
+	*n = *n >> index | bit;
+	*n = *n << index;
+	return (1);
+}

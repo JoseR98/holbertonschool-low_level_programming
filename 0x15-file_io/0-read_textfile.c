a3979224e5154a73eb02753b4740de23676e3d9e
JoseR98 <josealonsoingeniero@hotmail.com>
2020-03-31 22:29:47 +0000

    Add comments

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,23 +12,25 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int ret, fd;
+	/**Create buffer for read and write sycalls*/
 	char *buf;
 
+	/** buffer size is equal to the # of bits they ask for*/
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
 
 	if (filename == NULL)
 		return (0);
-
+	/**Open file for reading*/
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
+	/**Read sycall save n bits in the memory area buf*/
 	ret = read(fd, buf, letters);
 	if (ret == -1)
 		return (0);
-
+	/**Write sycall print n bits returned by Read from buf*/
 	write(STDOUT_FILENO, buf, ret);
 	free(buf);
 	close(fd);

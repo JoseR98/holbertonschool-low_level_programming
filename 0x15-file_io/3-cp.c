f0f14ba9ca01d04429508c9c18f584f6746ba388
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-01 04:25:23 +0000

    Change open permissions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,20 +7,19 @@
  */
 int main(int ac, char **av)
 {
-	int fd1, fd2, rd1, wf2, closed1, closed2;
+	int fd1, fd2, rd1 = 1024, wf2, closed1, closed2;
 	char buf1[1024];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	/**Open first file*/
-	fd1 = open(av[1], O_RDWR);
+	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	/**Open second file or create it*/
-	fd2 = open(av[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
+	fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	rd1 = 1024;
 	while (rd1 == 1024)
 	{
 		rd1 = read(fd1, buf1, 1024);

32e78151fc1b14c73fd81cb38bf198b5f725ead2
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-01 04:19:24 +0000

    Change read and write logic

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
 int main(int ac, char **av)
 {
 	int fd1, fd2, rd1, wf2, closed1, closed2;
-	char *buf1 = malloc(sizeof(char) * 1024);
+	char buf1[1024];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
@@ -16,19 +16,22 @@ int main(int ac, char **av)
 	fd1 = open(av[1], O_RDWR);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	/**Read the content of file_from*/
-	rd1 = read(fd1, buf1, 1024);
-	if (rd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	/**Open second file or create it*/
 	fd2 = open(av[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	/**Write content of file_from to file_to*/
-	wf2 = write(fd2, buf1, rd1);
-	if (wf2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	free(buf1);
+	rd1 = 1024;
+	while (rd1 == 1024)
+	{
+		rd1 = read(fd1, buf1, 1024);
+		if (rd1 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		wf2 = write(fd2, buf1, rd1);
+		if (wf2 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		if (rd1 != wf2)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
 	closed1 = close(fd1);
 	closed2 = close(fd2);
 	if (closed1 == -1)

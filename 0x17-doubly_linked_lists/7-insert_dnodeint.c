fb0a2caa38eb68528c72a948b61a4298887f13d2
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-02 20:13:28 +0000

    Fix adding node at the beginning

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - insert new node at a given index
  * @h: pointer to first node passed by reference
@@ -10,16 +9,23 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *temp = *h, *save_pos = NULL;
+	dlistint_t *temp = NULL, *save_pos = NULL;
 	dlistint_t *new_pos = malloc(sizeof(dlistint_t));
 
-	if (new_pos == NULL || *h == NULL)
+	if (new_pos == NULL || h == NULL || (*h == NULL && idx > 0))
 		return (NULL);
+	temp = *h;
 	new_pos->n = n;
+	new_pos->prev = NULL;
+	new_pos->next = NULL;
+	if (temp == NULL)
+	{
+		*h = new_pos;
+		return (new_pos);
+	}
 	if (idx == 0)
 	{
 		new_pos->next = *h;
-		new_pos->prev = NULL;
 		temp->prev = new_pos;
 		*h = new_pos;
 		return (new_pos);
@@ -34,11 +40,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp->next = new_pos;
 		}
 		temp = temp->next;
-	}
-	if (temp == NULL)
-	{
-		free(new_pos);
-		return (NULL);
 	}
 	temp->next = save_pos;
 	if (save_pos != NULL)

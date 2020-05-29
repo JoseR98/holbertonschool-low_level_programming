fbcf38e0350e827676fdd470ab98a864841dfb68
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-29 18:54:32 +0000

    Fix malloc for new node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,9 +17,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == NULL || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
@@ -33,6 +30,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
 	if (new->key == NULL || new->value == NULL)

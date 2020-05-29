360c4aea0271db7a20a7822ab4d60192ed412a24
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-29 15:03:51 +0000

    Verify if size is different to 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
 
-	new_table = malloc(sizeof(hash_table_t));
+	if (size == 0)
+		return (NULL);
+	new_table = calloc(size, sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;

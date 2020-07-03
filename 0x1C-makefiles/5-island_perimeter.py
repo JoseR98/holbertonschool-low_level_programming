5c6c4b151fc73f96025585af9c106ffd1d6cb84e
JoseR98 <josealonsoingeniero@hotmail.com>
2020-07-03 21:32:18 +0000

    FIX 6

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,4 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 """Island interpreter perimeter"""
 
 
@@ -12,29 +12,17 @@ def island_perimeter(grid):
     next_row = 0
     left_col = 0
     right_col = 0
-    row = 0
     perimeter = 0
-    while row < len(grid):
-        col = 0
-        while col < len(grid[row]):
-            actual_pos = grid[row][col]
+    row1 = 0
+    for row in grid:
+        col1 = 0
+        for col in row:
+            actual_pos = col
             if actual_pos == 1:
-                try:
-                    prev_row = grid[row - 1][col]
-                except Exception:
-                    prev_row = 0
-                try:
-                    next_row = grid[row + 1][col]
-                except Exception:
-                    next_row = 0
-                try:
-                    left_col = grid[row][col - 1]
-                except Exception:
-                    left_col = 0
-                try:
-                    right_col = grid[row][col + 1]
-                except Exception:
-                    right_col = 0
+                prev_row = grid[row1 - 1][col1]
+                next_row = grid[row1 + 1][col1]
+                left_col = grid[row1][col1 - 1]
+                right_col = grid[row1][col1 + 1]
                 if next_row == 0:
                     perimeter += 1
                 if prev_row == 0:
@@ -43,6 +31,6 @@ def island_perimeter(grid):
                     perimeter += 1
                 if right_col == 0:
                     perimeter += 1
-            col += 1
-        row += 1
+            col1 += 1
+        row1 += 1
     return perimeter

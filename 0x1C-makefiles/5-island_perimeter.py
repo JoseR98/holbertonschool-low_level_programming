17f4bdf8ec13b17f5b314e583002908fa82d3fb8
JoseR98 <josealonsoingeniero@hotmail.com>
2020-07-03 22:58:55 +0000

    Final fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,24 +13,12 @@ def island_perimeter(grid):
         for col in range(len(grid[row])):
             actual_pos = grid[row][col]
             if actual_pos == 1:
-                try:
-                    if grid[row - 1][col] == 0:
-                        perimeter += 1
-                except Exception:
+                if row - 1 == -1 or grid[row - 1][col] == 0:
                     perimeter += 1
-                try:
-                    if grid[row + 1][col] == 0:
-                        perimeter += 1
-                except Exception:
+                if row + 1 == len(grid) or grid[row + 1][col] == 0:
                     perimeter += 1
-                try:
-                    if grid[row][col - 1] == 0:
-                        perimeter += 1
-                except Exception:
+                if col - 1 == -1 or grid[row][col - 1] == 0:
                     perimeter += 1
-                try:
-                    if grid[row][col + 1] == 0:
-                        perimeter += 1
-                except Exception:
+                if col + 1 == len(grid[row]) or grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter

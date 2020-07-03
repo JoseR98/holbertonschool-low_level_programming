7cff5bbf75a388c0d35eb1a099ab58ea92fc64dd
JoseR98 <josealonsoingeniero@hotmail.com>
2020-07-03 21:13:53 +0000

    FIX 2

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,20 +13,23 @@ def island_perimeter(grid):
 	left_col = 0
 	right_col = 0
 	row = 0
-	col = 0
 	perimeter = 0
 	while row < len(grid):
+		col = 0
 		while col < len(grid[row]):
 			actual_pos = grid[row][col]
-			print(grid[row])
 			if actual_pos == 1:
 				prev_row = grid[row - 1][col]
 				next_row = grid[row + 1][col]
 				left_col = grid[row][col - 1]
 				right_col = grid[row][col + 1]
-				if prev_row == 0 or next_row == 0:
+				if next_row == 0:
 					perimeter += 1
-				if left_col == 0 or right_col == 0:
+				if prev_row == 0:
+					perimeter += 1
+				if left_col == 0:
+					perimeter += 1
+				if right_col == 0:
 					perimeter += 1
 			col += 1
 		row += 1

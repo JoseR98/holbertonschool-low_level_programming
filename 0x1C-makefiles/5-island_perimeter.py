bfc13d26a7d07818d368070c25002add7bf0f528
JoseR98 <josealonsoingeniero@hotmail.com>
2020-07-03 21:52:11 +0000

    FIX 7

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,21 +8,27 @@ def island_perimeter(grid):
     Args:
         grid ([list of lists]): grid with water and islands
     """
-    prev_row = 0
-    next_row = 0
-    left_col = 0
-    right_col = 0
     perimeter = 0
-    row1 = 0
-    for row in grid:
-        col1 = 0
-        for col in row:
-            actual_pos = col
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            actual_pos = grid[row][col]
             if actual_pos == 1:
-                prev_row = grid[row1 - 1][col1]
-                next_row = grid[row1 + 1][col1]
-                left_col = grid[row1][col1 - 1]
-                right_col = grid[row1][col1 + 1]
+                try:
+                    prev_row = grid[row - 1][col]
+                except Exception:
+                    prev_row = 0
+                try:
+                    next_row = grid[row + 1][col]
+                except Exception:
+                    next_row = 0
+                try:
+                    left_col = grid[row][col - 1]
+                except Exception:
+                    left_col = 0
+                try:
+                    right_col = grid[row][col + 1]
+                except Exception:
+                    right_col = 0
                 if next_row == 0:
                     perimeter += 1
                 if prev_row == 0:
@@ -31,6 +37,13 @@ def island_perimeter(grid):
                     perimeter += 1
                 if right_col == 0:
                     perimeter += 1
-            col1 += 1
-        row1 += 1
     return perimeter
+
+"""if grid[row - 1][col] == 0:
+                    perimeter += 1
+                if grid[row + 1][col] == 0:
+                    perimeter += 1
+                if grid[row][col - 1] == 0:
+                    perimeter += 1
+                if grid[row][col + 1] == 0:
+                    perimeter += 1"""
